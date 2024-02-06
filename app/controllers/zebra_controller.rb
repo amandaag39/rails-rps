82d2f05af06056fec5aff3d5e53bdd2a911e3938
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-02-06 21:14:14 +0000

    completed copy of sinatra version to rails and ran tests

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,51 @@
+class ZebraController < ApplicationController
+  def giraffe
+    @moves = ["rock", "paper", "scissors"]
+
+    @comp_move = @moves.sample
+  
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    render({ :template => "game_templates/play_rock"})
+  end
+
+  def elephant
+    @moves = ["rock", "paper", "scissors"]
+
+    @comp_move = @moves.sample
+
+    if @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    render( :template => "game_templates/play_paper")
+  end
+
+  def lion
+    @moves = ["rock", "paper", "scissors"]
+
+    @comp_move = @moves.sample
+  
+    if @comp_move == "scissors"
+      @outcome = "tied"
+    elsif @comp_move == "rock"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    render( :template => "game_templates/play_scissors") 
+  end
+
+  def bear
+    render( :template => "game_templates/home")
+  end
+
+end
